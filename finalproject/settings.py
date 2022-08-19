57acbbda2e2bd4274c47dc3f4bb720e70c296cb8
gilbertrothanglien <75418704+gilbertrothanglien@users.noreply.github.com>
2022-08-19 16:44:59 +0530

    Update settings.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -127,7 +127,7 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'imagesite/static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'imagesite/static'),
     )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
